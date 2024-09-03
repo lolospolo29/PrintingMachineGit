6ee2f22d693e3263154f6fec9dd8dbce66a7c6ed
heurich.lauris <heurich.lauris@mm-bbs.de>
2024-09-03 21:47:44 +0200

    * added server

---
+++ Server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+#rom Classes import AlertSignal, TradeSignal
+
+app = Flask(__name__)
+
+
+@app.route('/bot', methods=['POST'])
+def receive_signal():
+    #AlertSignal.reset_countdown()  # Countdown zurücksetzen
+    json_string = request.data.decode('utf-8')
+    #TradeSignal.order_signal(json_string)
+
+    return f'Received Analyse data: {json_string}'
+
+
+@app.route('/startcountdown', methods=['POST'])
+def startcountdown():
+    #AlertSignal.start_Alert_Thread()  # Countdown zurücksetzen
+    json_string = request.data.decode('utf-8')
+    return f'Received Analyse data: {json_string}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+#API Secrets
+#Name : DemoApiKey
+#Api Key : Ifp91Iy8fOY6x22bVG
+#Secret: eagubhmZG5nY0gbycEeJNGR50GuT5YnpZA37
