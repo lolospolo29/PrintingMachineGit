2b6b225712cff651e73d31e3df401bb9096d00cf
heurich.lauris <heurich.lauris@mm-bbs.de>
2024-09-22 22:59:56 +0200

    * added did a lot

---
+++ Models/DBModels/MongoDB.py
@@ -0,0 +1,107 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+from Interfaces.IDBService import IDBService
+
+
+class DBService(IDBService):
+    def __init__(self, db_name, uri):
+        """
+        Initialize the DbService with a MongoDB URI and database name.
+        """
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+
+    def add(self, collection_name, data):
+        # """
+        # Add a new document to a collection.
+        # param collection_name: The name of the collection.
+        # param data: A dictionary representing the document to insert.
+        # :return: The inserted document's ID.
+        # """
+        collection = self.db[collection_name]
+        result = collection.insert_one(data)
+        return str(result.inserted_id)
+
+    def get(self, collection_name, document_id):
+        """
+        Retrieve a document by its ID.
+        param collection_name: The name of the collection.
+        param document_id: The document's unique ID (as a string).
+        return: The document, or None if not found.
+        """
+        collection = self.db[collection_name]
+        return collection.find_one({"_id": ObjectId(document_id)})
+
+    def update(self, collection_name, document_id, updates):
+        # """
+        # Update an existing document in a collection.
+        # param collection_name: The name of the collection.
+        # param document_id: The document's unique ID (as a string).
+        # param updates: A dictionary with the fields to update.
+        # return: True if the update was successful, False otherwise.
+        # """
+        collection = self.db[collection_name]
+        result = collection.update_one({"_id": ObjectId(document_id)}, {"$set": updates})
+        return result.modified_count > 0
+
+    def delete(self, collection_name, document_id):
+        """
+        Delete a document by its ID.
+        param collection_name: The name of the collection.
+        param document_id: The document's unique ID (as a string).
+        return: True if the deletion was successful, False otherwise.
+        """
+        collection = self.db[collection_name]
+        result = collection.delete_one({"_id": ObjectId(document_id)})
+        return result.deleted_count > 0
+
+    def deleteByQuery(self, collection_name, query):
+        documents = self.find(collection_name, query)
+        deleted_ids = []
+
+        for document in documents:
+            document_id = str(document['_id'])
+            if self.delete(collection_name, document_id):
+                deleted_ids.append(document_id)
+
+    def delete_all(self, collection_name):
+        """
+        Delete all documents in the specified collection.
+
+        param collection_name: The name of the collection.
+        return: The number of documents deleted.
+        """
+        collection = self.db[collection_name]
+        result = collection.delete_many({})
+
+        # Return the count of deleted documents
+        return result.deleted_count
+
+    def find(self, collection_name, query):
+        """
+        Find documents in a collection based on a query.
+        param collection_name: The name of the collection.
+        param query: A dictionary representing the query (optional).
+        return: A list of matching documents.
+        """
+        if query is None:
+            query = {}
+        collection = self.db[collection_name]
+        return list(collection.find(query))
+
+    def execute_raw_query(self, collection_name, query):
+        """
+        Execute a raw MongoDB query.
+        param collection_name: The name of the collection.
+        param query: A MongoDB query.
+        return: The result of the query.
+        """
+        collection = self.db[collection_name]
+        return list(collection.find(query))
+
+    def close(self):
+        """
+        Close the MongoDB connection.
+        """
+        self.client.close()
